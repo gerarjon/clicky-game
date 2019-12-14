05fc12748d7f757f502f99bc354c780955a2a0d7
gerarjon <gerarsuaverdez@gmail.com>
2019-12-14 15:17:30 -0800

    Updates

---
+++ precache-manifest.06aa72d163d9121d2a3d18d623c2af69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e9a56ee97a451b17620848330f776a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "15b5a60bedeab9b5d371",
+    "url": "/clicky-game/static/css/main.093a23b7.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15b5a60bedeab9b5d371",
+    "url": "/clicky-game/static/js/main.76787a9b.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
